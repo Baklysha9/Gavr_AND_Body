7be808e6559f186b880d5584e32163b2953a501e
Baklysha9 <bogdancoo@yandex.ru>
2018-02-14 21:17:53 +0300

    one

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </head>
+  <body>
+  	<div class="bod">
+  		<header>
+  			<div class="container-fluid head">
+  				<div class="col-md-3"></div>
+  				<div class="col-md-6"><!--Лого--></div>
+  				<div class="col-md-3">
+  					<button class="login">Войти</button>
+  					<button class="login">Регистрация</button>
+  				</div>
+			 </div>
+  		</header>
+  		<section class="sect" >
+  			<div class="container-fluid">
+          <div class="col-md-1"></div>
+          <div class="col-md-10">
+            <p class="title">Билеты на самолет</p>
+          </div>
+          <div class="col-md-1"></div>
+       </div>
+       <div class="container-fluid">
+          <div class="col-md-1"></div>
+          <div class="col-md-10">
+            <form> 
+              <label>Откуда
+              <input type="text" class="frm" name=""></label>
+              <label>Куда
+              <input type="text" class="frm"  name=""></label>
+              <label>Вылет
+              <input type="date" class="frm"  name=""></label>
+              <label>Прилет
+              <input type="date" class="frm" name=""></label>
+              <label>Количество
+              <input type="text" class="frm" name=""></label>
+              <div class="sub">
+              <input type="submit" name="">
+            </div>
+            </form>
+          </div>
+          <div class="col-md-1"></div>
+       </div>
+  		</section>
+
+  	</div>
+ 	
+ 	<footer>
+ 		
+ 	</footer>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
+</html>
